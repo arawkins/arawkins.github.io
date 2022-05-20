8166122e0b76c4e219173c783fd31023af39e0e7
Alan Rawkins <arawkins@gmail.com>
2022-05-20 10:13:28 -0700

    Moved re-usable template components to includes, updated template extensions to .liquid

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,13 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("gfx");
+    eleventyConfig.addPassthroughCopy("js");
+
+    return  {
+        dir: {
+            //input: "src",
+            includes: "_includes",
+            layouts: "_layouts"
+        }
+    }
 };
